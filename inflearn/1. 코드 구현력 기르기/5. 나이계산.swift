836b866cc8a5b95a9a2898f8f3be3ed0dd5d0554
Han Seung Rae <31765530+hhhan0315@users.noreply.github.com>
2021-03-30 11:55:31 +0900

    Create 5. 나이계산.swift

---
+++ inflearn/1. 코드 구현력 기르기/5. 나이계산.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+func solution(_ str: String) -> String {
+    let num = str.components(separatedBy: "-")
+    let ageFirstIndex = num[0].index(after: num[0].startIndex)
+    let ageFirst = num[0][num[0].startIndex...ageFirstIndex]
+    let genderFirst = num[1].first!
+    var age = 0
+    var gender = ""
+    
+    switch genderFirst {
+    case "1":
+        gender = "M"
+        age = 1900
+    case "2":
+        gender = "W"
+        age = 1900
+    case "3":
+        gender = "M"
+        age = 2000
+    case "4":
+        gender = "W"
+        age = 2000
+    default:
+        break
+    }
+    
+    age += Int(ageFirst)!
+    age = 2019 - age + 1
+    print(age, gender)
+    
+    return ""
+}
